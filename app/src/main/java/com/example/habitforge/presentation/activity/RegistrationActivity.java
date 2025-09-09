9e974b2517bdd7b45bc48b14421f7b8f6200c32b
Irina Poposki <ninapoposki6@gmail.com>
2025-09-09 20:49:02 +0200

    manage merging conflicts

--- app/src/main/java/com/example/habitforge/presentation/activity/RegistrationActivity.java
+++ app/src/main/java/com/example/habitforge/presentation/activity/RegistrationActivity.java
@@ -1,4 +1,4 @@
-//package com.example.habitforge.presentation.activity;
+package com.example.habitforge.presentation.activity;
 //
 //import android.os.Bundle;
 //
@@ -24,19 +24,38 @@
 ////        });
 ////    }
 //
-//import android.os.Bundle;
-//import android.widget.Button;
-//import android.widget.EditText;
-//import android.widget.Toast;
-//
-//import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.habitforge.R;
+
 //
 //import com.example.habitforge.R;
 //import com.example.habitforge.application.service.UserService;
 //import com.google.android.gms.tasks.OnCompleteListener;
 //import com.google.firebase.auth.AuthResult;
 //
-//    public class RegistrationActivity extends AppCompatActivity {
+    public class RegistrationActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_registration);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    }
 //
 //        private EditText etEmail, etPassword, etRepeatPassword, etUsername;
 //        private Button btnRegister;
